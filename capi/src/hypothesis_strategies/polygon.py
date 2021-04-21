923afe27f22211f528dd0be9441a6f6f047f15ac
James Balajan <James.Balajan@wisetechglobal.com>
2021-04-21 16:54:10 +1000

    - Freeze coordinate

--- capi/src/hypothesis_strategies/polygon.py
+++ capi/src/hypothesis_strategies/polygon.py
@@ -8,5 +8,5 @@ from capi.src.implementation.datastructures.polygon import Polygon
 
 @st.composite
 def polygon_strategy(draw: typing.Callable, min_num_vertices: int = 3, max_num_vertices: int = 10):
-    vertices = draw(st.lists(coordinate_strategy(), min_size=min_num_vertices, max_size=max_num_vertices))
+    vertices = draw(st.lists(coordinate_strategy(), min_size=min_num_vertices, max_size=max_num_vertices, unique=True))
     return Polygon(vertices)
