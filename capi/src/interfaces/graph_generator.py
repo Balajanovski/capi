cec3e16410d1a6af9d5e2675386375aa1928241e
James Balajan <James.Balajan@wisetechglobal.com>
2021-05-12 17:24:55 +1000

    - Ranges

--- capi/src/interfaces/graph_generator.py
+++ capi/src/interfaces/graph_generator.py
@@ -5,3 +5,7 @@ class IGraphGenerator(abc.ABC):
     @abc.abstractmethod
     def generate(self, shape_file_path: str, output_path: str) -> None:
         pass
+
+    @abc.abstractmethod
+    def generate_for_vertex_range(self, shape_file_path: str, output_path: str, current_split_num: int, num_splits: int, seed: int) -> None:
+        pass
