81e5afc84332a08905172dce1845ca302f75566f
James Balajan <James.Balajan@wisetechglobal.com>
2021-05-31 11:00:27 +1000

    - Fix header guards

--- capi/src/implementation/visibility_graphs/code/constants/constants.hpp
+++ capi/src/implementation/visibility_graphs/code/constants/constants.hpp
@@ -5,8 +5,6 @@
 #ifndef CAPI_CONSTANTS_HPP
 #define CAPI_CONSTANTS_HPP
 
-#endif // CAPI_CONSTANTS_HPP
-
 static constexpr double EPSILON_TOLERANCE = 0.0000000000001;
 static constexpr double EPSILON_TOLERANCE_SQUARED = EPSILON_TOLERANCE * EPSILON_TOLERANCE;
 static constexpr double MAX_LONGITUDE = 180.0;
@@ -15,3 +13,5 @@ static constexpr double LONGITUDE_PERIOD = MAX_LONGITUDE - MIN_LONGITUDE + (EPSI
 static constexpr double MAX_PERIODIC_LONGITUDE = MAX_LONGITUDE + LONGITUDE_PERIOD;
 static constexpr double MAX_LATITUDE = 90.0;
 static constexpr double MIN_LATITUDE = -90.0;
+
+#endif // CAPI_CONSTANTS_HPP
