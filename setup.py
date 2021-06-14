9a84c00a2ec5907c701355c54547ff76e47500ca
James Balajan <James.Balajan@wisetechglobal.com>
2021-06-15 09:35:25 +1000

    - Revert fetchcontent changes

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open("requirements.txt", "r") as handle:
 
 setup(
     name="capi-vis",
-    version="2.3.0",
+    version="2.3.1",
     author="James Balajan",
     author_email="James.Balajan@wisetechglobal.com",
     description="Python library for performing coast aware path interpolation using visibility graphs",
