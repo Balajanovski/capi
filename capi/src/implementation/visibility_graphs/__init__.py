30c2b12e0048ec4644072bbf80c145e21ae00f4a
James Balajan <James.Balajan@wisetechglobal.com>
2022-04-04 09:32:55 +1000

    - Fix tests

--- capi/src/implementation/visibility_graphs/__init__.py
+++ capi/src/implementation/visibility_graphs/__init__.py
@@ -1,1 +1,14 @@
-from capi.src.implementation.visibility_graphs._vis_graph import *  # type: ignore
+from capi.src.implementation.visibility_graphs._vis_graph import (  # type: ignore
+    VisGraph,
+    VisGraphBatchInterpolateResult,
+    VisGraphCoord,
+    VisGraphPolygon,
+    VisGraphShortestPathComputer,
+    VisGraphVisibleVertex,
+    VistreeGenerator,
+    generate_visgraph,
+    generate_visgraph_with_shuffled_range,
+    load_graph_from_file,
+    merge_graphs,
+    save_graph_to_file,
+)
